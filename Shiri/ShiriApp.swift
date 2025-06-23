f16e20ff44ae818210393f3f2302d73d831abf67
Michel Sedgh <michelsedgh@gmail.com>
2025-06-23 14:46:25 -0700

    Initial Commit

---
+++ Shiri/ShiriApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShiriApp.swift
+//  Shiri
+//
+//  Created by Michel  on 6/23/25.
+//
+
+import SwiftUI
+
+@main
+struct ShiriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
